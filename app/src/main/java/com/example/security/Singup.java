89d6b92bce407f43913d87d0a61a619a4dccda32
ELMAJOULI ABDELWAHED <aelmajouli@gmail.com>
2020-07-15 15:19:28 +0100

    fisnish

--- app/src/main/java/com/example/security/Singup.java
+++ app/src/main/java/com/example/security/Singup.java
@@ -1,13 +1,46 @@
 package com.example.security;
 
+import android.content.Intent;
+import android.text.TextUtils;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.tasks.Task;
 
 public class Singup extends AppCompatActivity {
+
+    private TextView tv_email;
+    private TextView tv_pass;
+    private FirbaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singup);
+        tv_email = findViewById(R.id.username_et);
+        tv_pass = findViewById(R.id.password1_et);
+    }
+
+    public void registerFirbase(){
+        String email = tv_email.getText().toString();
+        String pass = tv_pass.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            tv_email.setError("Email is Required");
+            return;
+        }
+        if (TextUtils.isEmpty(email)){
+            tv_pass.setError("Pass is Required");
+            return
+        }
+
+        fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListner(new on CompleteListner<AuthResult>)(){
+            @Override
+            public  vod OnComplete(Task<AuthREsult> task){
+            startActivity(new Intent(getApplicationContext(),LoginActivity.class)) ;
+            }
+        }
+
     }
 }
